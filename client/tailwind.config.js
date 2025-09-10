2b9c5a6c263868b72b084a69e8f5b945c2857a33
Raman9937 <39644356+Raman9937@users.noreply.github.com>
2025-09-11 02:23:29 +0530

    fixes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -67,44 +67,24 @@ module.exports = {
           to: { height: "0" },
         },
         "fade-in": {
-          "0%": {
-            opacity: "0",
-          },
-          "100%": {
-            opacity: "1",
-          },
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
         "fade-out": {
-          "0%": {
-            opacity: "1",
-          },
-          "100%": {
-            opacity: "0",
-          },
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
         },
         "slide-in-from-top": {
-          "0%": {
-            transform: "translateY(-100%)",
-          },
-          "100%": {
-            transform: "translateY(0)",
-          },
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
         },
         "slide-out-to-top": {
-          "0%": {
-            transform: "translateY(0)",
-          },
-          "100%": {
-            transform: "translateY(-100%)",
-          },
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-100%)" },
         },
         shimmer: {
-          from: {
-            backgroundPosition: "0 0",
-          },
-          to: {
-            backgroundPosition: "-200% 0",
-          },
+          from: { backgroundPosition: "0 0" },
+          to: { backgroundPosition: "-200% 0" },
         },
         spotlight: {
           "0%": {
